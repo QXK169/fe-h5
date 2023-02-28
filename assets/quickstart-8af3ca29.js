c63684cfd368bec838a7ff91e0bb8d8a40e1190e
zhen.wang1 <zhen.wang@saloontech.com>
2023-02-28 18:47:22 +0800

    Updates

---
+++ assets/quickstart-8af3ca29.js
@@ -0,0 +1,9 @@
+import{o as a,a as s,z as e}from"./vue-libs-3cddcbbd.js";const t={class:"van-doc-markdown-body"},c=e(`<h1>快速上手</h1><div class="van-doc-card"><h3 id="an-zhuang" tabindex="-1">安装</h3><pre><code class="language-bash"><span class="hljs-comment"># 通过 npm</span>
+npm i fe-H5
+
+<span class="hljs-comment"># 通过 yarn</span>
+yarn add fe-H5
+
+<span class="hljs-comment"># 通过 pnpm</span>
+pnpm add fe-H5
+</code></pre></div>`,2),o=[c],_={__name:"quickstart",setup(d,{expose:n}){return n({frontmatter:{}}),(p,m)=>(a(),s("div",t,o))}};export{_ as default};
